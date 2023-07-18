619f2bd6f8324324c6be94a4e0425cc71b8a7626
Elena Vasileva <123897353+Elena763@users.noreply.github.com>
2023-07-18 18:04:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,56 @@
-//const title = document.getElementById("titlelist");
+//Основные переменные
 const divforlist = document.getElementById("divforlist");
-const button = document.getElementById("formbtn");
-const tasks =[];
-class Task {
-    constructor(to) {
-    this.to = to;
-    }
-    check(){
-        this.isChecked = !this.isChecked;
-    }
+let button = document.getElementById("formbtn");
+let input = document.querySelector('#forminput');
+let clearButton = document.querySelector('.clear_btn');
+//Задание массива задач
+let todoList =[];
+//Вытаскиваем данные массива из Local Storage
+if(localStorage.getItem('todo')){
+    todoList = JSON.parse(localStorage.getItem('todo'));
+    addTodoList();
 }
-
-function addNewTask(event) {
+//Слушатель на кнопку добавления задачи, создаем объект массива,
+//сохраняем в Local Storage, очищаем поле ввода
+button.addEventListener("click",function (event) {
     event.preventDefault();
-    let input = document.getElementById("forminput").value;
-    let uniqueId = "id" + Date.now();
-
-    let newDiv = document.createElement('div');
-    newDiv.innerHTML = `<div class="list">
-    <p class="list_p" id="` + `${uniqueId}` + `">Задача</p>
-    <input class="list_check" type="checkbox">
-    </div>`;
-    divforlist.appendChild(newDiv);
-
-    let newTask = document.getElementById(`${uniqueId}`);
-    newTask.textContent = input;
-    const task = new Task (newTask);
-    console.log(task);
-    tasks.push(task);
-    //localStorage.setItem('tasks', JSON.stringify(tasks));
-    //console.log(JSON.stringify(tasks));
+    if(!input.value) return;
+    let newTodo = {
+        todo: input.value,
+        checked: false,
+    };
+    todoList.push(newTodo);
+    addTodoList();
+    localStorage.setItem('todo', JSON.stringify(todoList));
+    input.value = '';
+});
+//Функция добавления задачи в список задач
+function addTodoList() {
+    let newDiv = "";
+    todoList.forEach(function(item,i){
+        newDiv += `<div class="list">
+        <p class="list_p" id="p_check_${i}">${item.todo}</p>
+        <input class="list_check" id="check_${i}" type="checkbox" ${item.checked ? "checked" : ""}>
+        </div>`;
+        divforlist.innerHTML = newDiv;
+    });
 }
+//Слушатель чекбокса
+divforlist.addEventListener('change', function(event){
+    let idInput = event.target.getAttribute('id');
+    let valueP = divforlist.querySelector('#p_' + idInput).innerHTML;
 
-button.addEventListener("click", addNewTask);
+    todoList.forEach(function (item) {
+        if (item.todo === valueP){
+            item.checked = !item.checked;
+            localStorage.setItem('todo', JSON.stringify(todoList));
+        }
+    });
+});
+//Слушатель кнопка очищени спика задач
+clearButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    localStorage.clear();
+    todoList = [];
+    divforlist.innerHTML = 'Нет задач';
+})
